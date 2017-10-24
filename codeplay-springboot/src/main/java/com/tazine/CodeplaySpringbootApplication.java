5faa2f9af8dfa0a44e1bb147f7a377a5e03250b6
Frank <bookfrank@foxmail.com>
2017-10-24 22:05:02 +0800

    springboot中实现java的tail -f功能

---
+++ codeplay-springboot/src/main/java/com/tazine/CodeplaySpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.tazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeplaySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeplaySpringbootApplication.class, args);
+	}
+}

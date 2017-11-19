ff42969af0db95d1eb0f9ae218889dede7d9823b
Frank <bookfrank@foxmail.com>
2017-11-19 23:48:24 +0800

    Netty-TimeServer Example

---
+++ codeplay-io/src/main/java/com/tazine/netty/TimeServer.java
@@ -0,0 +1,53 @@
+package com.tazine.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.io.IOException;
+
+/**
+ * Created on 2017/11/19.
+ * @author Frank
+ */
+public class TimeServer {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    private void bind(int port) throws IOException{
+
+        // 配置服务端的 NIO 线程组
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024);
+
+            // 绑定端口，同步等待成功
+            ChannelFuture channelFuture = bootstrap.bind(port).sync();
+
+            // 等待服务端监听端口关闭
+            channelFuture.channel().close().sync();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+
+}

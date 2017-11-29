adb2d3be3d1890ebdb38dc8520ea0ad8c7be588c
Frank <bookfrank@foxmail.com>
2017-11-29 23:51:21 +0800

    Thread vs Runnable

--- codeplay-thread/src/main/java/com/tazine/thread/VolatileDemo.java
+++ codeplay-thread/src/main/java/com/tazine/thread/VolatileDemo.java
@@ -1,21 +1,31 @@
 package com.tazine.thread;
-import java.util.concurrent.*;
+
 /**
  * Volatile关键词：能够实现可见性，但是无法实现原子性
  * @author Frank
  */
 public class VolatileDemo {
 
-    private volatile int result = 0;
+    private static int result = 10;
 
     public static void main(String[] args) {
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
+        int i = 10;
+        while (i-- > 0){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName() + "-当前result值为" + result);
+                    result--;
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
 
-            }
-        }).start();
 
     }
 

2325f04a06e99c1946f0468e89d4944a061fdd9f
Frank <bookfrank@foxmail.com>
2017-12-01 21:23:42 +0800

    char 相关内容积累

---
+++ codeplay-encode/src/main/java/com/tazine/encode/datetype/CoolChar.java
@@ -0,0 +1,53 @@
+package com.tazine.encode.datetype;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ *
+ * 有关 char 的那些事
+ * @author Frank
+ */
+public class CoolChar {
+
+
+    public static void main(String[] args) throws UnsupportedEncodingException {
+
+        CoolChar coolChar = new CoolChar();
+
+        coolChar.charDisplay();
+
+    }
+
+
+    /**
+     * char 的两种表示方法
+     */
+    private void charDisplay() throws UnsupportedEncodingException {
+
+        char a = '\u4e2d';
+        char b = '中';
+        String c = "中";
+
+        System.out.println("char:");
+        System.out.println("   " + a + " 打印为：" + a + ", unicode = " + (int) a + "，对应的16进制数为 " + Integer.toHexString(a));
+        System.out.println("   \\u4e2d 打印为 ：" + b + ", unicode = " + (int) b + "，对应的16进制数为 " + Integer.toHexString(b));
+        System.out.println("String:");
+        System.out.print("   中 字符串的Unicode编码字节数组为：");
+        for (byte bt : c.getBytes("Unicode")){
+            System.out.print( Integer.toHexString(bt & 0xff) + " ");
+            // byte & 0xff 是将有符号的字节转为无符号的整数
+        }
+        System.out.println();
+        // 可以看出 中 无论是在数据类型char 还是 String 其编码是一致的 （char 默认为 unicode编码）
+
+        char[] charArr = c.toCharArray();
+        for (char i : charArr){
+            System.out.println(i);
+        }
+    }
+
+
+
+
+
+}

229445bbe3c60008d040dadcf55c0ae25608712c
Frank <bookfrank@foxmail.com>
2017-12-02 23:41:48 +0800

    java int 强制转换为 byte

--- codeplay-encode/src/main/java/com/tazine/encode/ByteTrancute.java
+++ codeplay-encode/src/main/java/com/tazine/encode/ByteTrancute.java
@@ -9,11 +9,38 @@ import java.io.UnsupportedEncodingException;
 public class ByteTrancute {
 
 
+    /**
+     * int 类型强制转换为 byte
+     */
+    private void intToByte(){
+
+        byte a = (byte)127;
+        System.out.println("(byte)127 = " + a);
+        // =127 因为 byte 类型的取值范围为 -128~127 在这个范围内都可以正常转换
+
+        byte b = (byte)128;
+        System.out.println("(byte)128 = " + b);
+        // =   因为整型的128的为 00000000 00000000 00000000 10000000，截取低8位为 10000000
+        // 因为截取的 10000000 是负数，求原码得 11111111 + 1 = 1 0000 0000 视为 -128
+
+        byte c = (byte)300;
+        System.out.println("(byte)300 = " + c);
+
+        byte d = (byte) -300;
+        System.out.println("(byte) -300 = " + d);
+
+        int x = 0xff;
+        byte e = (byte)0x80;
+
+        System.out.println(a + " " + b + " " + c + " " + d + " " + e);
+    }
+
+
     public static void main(String[] args) throws UnsupportedEncodingException {
 
         byte a = 127;
         byte b = (byte)128;
-        byte c = (byte)200;
+        byte c = (byte) -300;
 
         int x = 0xff;
         byte d = (byte)x;

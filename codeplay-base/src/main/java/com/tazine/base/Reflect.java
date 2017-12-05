98f31693e20c3dcffc38844ed4031d99993f38cd
Frank <bookfrank@foxmail.com>
2017-12-05 23:45:28 +0800

    Java 反射的使用

---
+++ codeplay-base/src/main/java/com/tazine/base/Reflect.java
@@ -0,0 +1,28 @@
+package com.tazine.base;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * Created on 2017/12/5.
+ */
+public class Reflect {
+
+
+    public static void main(String[] args) {
+
+        BitOperator bitOperator = new BitOperator();
+
+        Class ref = bitOperator.getClass();
+
+        for (Method method : ref.getDeclaredMethods()){
+            System.out.println(method.getName());
+        }
+
+        for (Field field : ref.getDeclaredFields()){
+            System.out.println(field.getName());
+        }
+
+    }
+
+}

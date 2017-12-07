5c110fb1536e1f9d31053ef4261b6dedfdd8d169
Frank <bookfrank@foxmail.com>
2017-12-07 22:55:29 +0800

    删除多余测试类

--- codeplay-base/src/main/java/com/tazine/App.java
+++ codeplay-base/src/main/java/com/tazine/App.java
@@ -1,5 +1,9 @@
 package com.tazine;
 
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Iterator;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        Hashtable<String,Integer> h = new Hashtable<String, Integer>();
+
+        h.put("a",1);
+        h.put("b",2);
+        h.put("c",3);
+        h.put("d",4);
+
+        Enumeration a = h.elements();
+        while (a.hasMoreElements()){
+            System.out.println(a.nextElement());
+        }
     }
 }

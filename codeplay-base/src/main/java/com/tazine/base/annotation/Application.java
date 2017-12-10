fc06824f1fd26cc9d98f52757740eab0260b5097
Frank <bookfrank@foxmail.com>
2017-12-10 22:33:33 +0800

    feat (annotation) : @Autowired annotation works, wait to prefect
    Using the reflect to complete the autowired
    
    Keep calm and carry on.

--- codeplay-base/src/main/java/com/tazine/base/annotation/Application.java
+++ codeplay-base/src/main/java/com/tazine/base/annotation/Application.java
@@ -1,5 +1,8 @@
 package com.tazine.base.annotation;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+
 /**
  * 自定义注解测试类
  *
@@ -11,14 +14,34 @@ public class Application {
     @Autowired
     private static Environment env;
 
-    public static void main(String[] args) {
+    private int count;
 
+    public static void main(String[] args) {
+        Application app = new Application();
+        parseAnnotations(app);
+        System.out.println();
+        System.out.println(env.getProperty("123"));
     }
 
-    public static void parseAnnotations(){
+    public static void parseAnnotations(Application application){
 
-        
+        Class app = application.getClass();
 
+        for (Field field : app.getDeclaredFields()){
+            System.out.print("当前字段名为" + field.getName() + " 类型为：" + field.getType());
+            Annotation[] annotations = field.getDeclaredAnnotations();
+            for (Annotation annotation : annotations){
+                if (annotation.annotationType() == Autowired.class){
+                    try {
+                        field.set("env", new Environment());
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.print(" 注解为：" + annotation.annotationType().getName());
+            }
+            System.out.println();
+        }
     }
 
 }

0a60d7dc0a6b6ce234ef4e57bf65b14f658e62e1
Frank <bookfrank@foxmail.com>
2017-12-14 22:51:05 +0800

    refactor (codeplay-thread) : Refactor the daemon thread.
    Change the file output to System.out.println().
    
    Practice makes perfect.

--- codeplay-thread/src/main/java/com/tazine/thread/daemon/DaemonThreadDemo.java
+++ codeplay-thread/src/main/java/com/tazine/thread/daemon/DaemonThreadDemo.java
@@ -1,8 +1,5 @@
 package com.tazine.thread.daemon;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -11,7 +8,7 @@ import java.util.Scanner;
  * @author Frank
  * @since 1.0.0
  */
-public class MainThread {
+public class DaemonThreadDemo {
 
     public static void main(String[] args) {
 
@@ -24,6 +21,7 @@ public class MainThread {
         if (scanner.hasNext()) {
             System.out.println(scanner.next());
         }
+        // scanner.hasNext()会使main线程阻塞在等待输入，输入后执行完毕守护线程也会终止。
     }
 }
 
@@ -34,35 +32,23 @@ class DaemonThread extends Thread {
 
     private String name;
 
-    public DaemonThread(String name) {
+    DaemonThread(String name) {
         this.name = name;
     }
 
     @Override
     public void run() {
-        File file = new File("/Users/lina/dsp/daemon.txt");
-        FileOutputStream outputStream = null;
         int i = 0;
         System.out.println("线程开始执行");
         try {
-            outputStream = new FileOutputStream(file);
             while (i < 100) {
                 System.out.println("打印内容：" + name.concat(":").concat(String.valueOf(i)));
-                outputStream.write("\r\n".concat(name).concat(":").concat(String.valueOf(i)).getBytes());
                 i++;
-                sleep(1000);
+                sleep(1000L);
             }
             System.out.println("线程结束执行");
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            if (null != outputStream) {
-                try {
-                    outputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 }

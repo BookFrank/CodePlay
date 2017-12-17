d4f5512eebd80193ee2fd44aeade3b3b85122599
Frank <bookfrank@foxmail.com>
2017-12-17 23:23:20 +0800

    fix (codeplay) : Modify the .gitignored files.

---
+++ codeplay-design-pattern/src/main/java/com/tazine/design/observer/ConcreteObserver.java
@@ -0,0 +1,18 @@
+package com.tazine.design.observer.target;
+
+import com.tazine.design.observer.Observer;
+
+/**
+ * 具体观察者对象：实现更新的方法，是订阅目标与自身状态保持一致
+ * Created by lina on 2017/12/17.
+ */
+public class ConcreteObserver implements Observer {
+    @Override
+    public void update() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ codeplay-design-pattern/src/main/java/com/tazine/design/observer/Observer.java
@@ -0,0 +1,9 @@
+package com.tazine.design.observer;
+
+/**
+ * 观察者接口：提供一个更新接口给那些有消息来时要通知的对象
+ * Created by lina on 2017/12/17.
+ */
+public interface Observer {
+    void update();
+}

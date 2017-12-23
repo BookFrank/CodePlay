b42e55b4bf07c206d3786a7169459d4b80ec9f3d
Frank <bookfrank@foxmail.com>
2017-12-23 23:55:59 +0800

    test (codeplay-java8) : Commit the java8 module test file.

--- codeplay-java8/src/main/java/com/tazine/java8/lambda/Test.java
+++ codeplay-java8/src/main/java/com/tazine/java8/lambda/Test.java
@@ -1,8 +1,6 @@
 package com.tazine.java8.lambda;
 
-import java.time.Clock;
 import java.util.Arrays;
-import java.util.Base64;
 
 /**
  * Created by lina on 2017/12/14.
@@ -13,10 +11,5 @@ public class Test {
     public static void main(String[] args) {
 
         Arrays.asList(1, 2, 3).forEach(System.out::println);
-
-        final Clock clock = Clock.systemUTC();
-
-        System.out.println(clock.instant());
-
     }
 }

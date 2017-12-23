8547f28b26b034fd406930d858a6d340201efd82
Frank <bookfrank@foxmail.com>
2017-12-23 23:54:19 +0800

    test (codeplay-algorithm) : Commit the test file.

--- codeplay-algorithm/src/main/java/com/tazine/algorithm/Test.java
+++ codeplay-algorithm/src/main/java/com/tazine/algorithm/Test.java
@@ -13,19 +13,20 @@ public class Test {
         int[] a = {2, 1, 4, 3, 5};
 
         System.out.println(Arrays.toString(a));
-        for(int i=1; i<a.length; i++){
 
-            for (int j=0; j<a.length-i; j++){
 
+        for (int i=1; i<a.length; i++){
+
+            for(int j=0; j<a.length -i; j++){
                 if (a[j] > a[j+1]){
                     int tmp = a[j];
                     a[j] = a[j+1];
-                    a[j+1] = tmp;
+                    a[j + 1] = tmp;
                 }
-
             }
 
         }
+
 
         System.out.println(Arrays.toString(a));
 

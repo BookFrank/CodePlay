b7ab394ba0a462c0dc2b80921225f95fbfb725a5
Frank <bookfrank@foxmail.com>
2017-12-24 23:17:55 +0800

    feat (codeplay-base) : Practice all the functions in the ClassLoader and Class Object.
    Practice makes perfect.

---
+++ codeplay-base/src/main/java/com/tazine/base/classes/ClassActions.java
@@ -0,0 +1,21 @@
+package com.tazine.base.classes;
+
+/**
+ * Class类中的所有方法
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class ClassActions {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(ClassActions.class.getResource(""));
+        System.out.println(ClassActions.class.getResource("/"));
+
+    }
+
+
+
+}

5337f2ecd5a73db460a5d8a3769a61c3f5f167fd
Frank <bookfrank@foxmail.com>
2017-12-25 21:59:40 +0800

    feat (codeplay-base) : Add practices in Classes and Loader package.
    Need more examples and practices.
    
    Practice makes perfect.

--- codeplay-base/src/main/java/com/tazine/base/classes/ClassActions.java
+++ codeplay-base/src/main/java/com/tazine/base/classes/ClassActions.java
@@ -1,5 +1,11 @@
 package com.tazine.base.classes;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Enumeration;
+
 /**
  * Class类中的所有方法
  *
@@ -8,14 +14,50 @@ package com.tazine.base.classes;
  */
 public class ClassActions {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        forName();
 
+        // 当前类class文件所在的包目录
         System.out.println(ClassActions.class.getResource(""));
+        // class path
         System.out.println(ClassActions.class.getResource("/"));
 
+        // class path
+        System.out.println(ClassActions.class.getClassLoader().getResource(""));
+
+        URL url = ClassActions.class.getResource("ClassDemo.class");
+        System.out.println(url.getPath());
+
+        Enumeration<URL> urls = ClassActions.class.getClassLoader().getResources("com.tazine.base".replace(".", "/"));
+        if (null == urls || !urls.hasMoreElements()) {
+            System.out.println("null");
+        }
+        while (urls.hasMoreElements()) {
+            System.out.println(urls.nextElement().getPath());
+        }
+
+        System.out.println();
+        File file = new File("/usr/local/Cellar/tomcat/8.5.14/libexec/webapps/examples/index.html");
+        try {
+            System.out.println("URI -- " + file.toURI());
+            System.out.println("URL -- " + file.toURI().toURL());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 
-
+    /**
+     * Class.forName() 默认初始化
+     */
+    private static void forName() {
+        try {
+            // Class.forName("com.tazine.base.classes.ClassUtils");
+            Class.forName("com.tazine.base.classes.ClassUtils", false, Thread.currentThread().getContextClassLoader());
+            // Class.forName() 默认会为Class进行初始化，初始化Class会执行static代码块和staic变量初始化
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
---
+++ codeplay-base/src/main/java/com/tazine/base/classes/ClassUtils.java
@@ -0,0 +1,13 @@
+package com.tazine.base.classes;
+
+/**
+ * Class 相关工具
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class ClassUtils {
+    static {
+        System.out.println("This is ClassUtils");
+    }
+}

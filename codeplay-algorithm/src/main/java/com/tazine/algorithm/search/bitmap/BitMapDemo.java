d6ae701766807d91b6689d052aaf216777b8c678
Frank <bookfrank@foxmail.com>
2018-01-06 23:44:10 +0800

    feat (codeplay-algorithm) : add the bitmap sort practice.

---
+++ codeplay-algorithm/src/main/java/com/tazine/algorithm/search/bitmap/BitMapDemo.java
@@ -0,0 +1,27 @@
+package com.tazine.algorithm.search.bitmap;
+
+import java.math.BigInteger;
+
+/**
+ * Created by lina on 2018/1/5.
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class BitMapDemo {
+
+    public static void main(String[] args) {
+
+        int[] phones = {59339456, 59339178};
+
+
+        int[] bitmap = new int[99999999];
+
+        for (int i=0;i<bitmap.length;i++){
+            System.out.println(bitmap[i]);
+        }
+
+    }
+
+
+}

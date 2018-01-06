0bae62827fa49e90d53cc19829d82c798d3ddb47
Frank <bookfrank@foxmail.com>
2018-01-06 23:44:46 +0800

    feat (codeplay-container) : add the BlockingQueue practice.

---
+++ codeplay-container/src/main/java/com/tazine/container/queue/block/BlockingQueueDemo.java
@@ -0,0 +1,139 @@
+package com.tazine.container.queue.block;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by lina on 2017/12/29.
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class BlockingQueueDemo implements BlockingQueue {
+    @Override
+    public boolean add(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean offer(Object o) {
+        return false;
+    }
+
+    @Override
+    public Object remove() {
+        return null;
+    }
+
+    @Override
+    public Object poll() {
+        return null;
+    }
+
+    @Override
+    public Object element() {
+        return null;
+    }
+
+    @Override
+    public Object peek() {
+        return null;
+    }
+
+    @Override
+    public void put(Object o) throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean offer(Object o, long timeout, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public Object take() throws InterruptedException {
+        return null;
+    }
+
+    @Override
+    public Object poll(long timeout, TimeUnit unit) throws InterruptedException {
+        return null;
+    }
+
+    @Override
+    public int remainingCapacity() {
+        return 0;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+
+    @Override
+    public int drainTo(Collection c) {
+        return 0;
+    }
+
+    @Override
+    public int drainTo(Collection c, int maxElements) {
+        return 0;
+    }
+}

e257ff5e7f7de9adf76450bc962a08166af2f47f
Frank <bookfrank@foxmail.com>
2018-01-10 22:35:30 +0800

    feat (codeplay-io) : FileChannel and RandomAccessFile practice.
    Practice makes perfect.

---
+++ codeplay-io/src/main/java/com/tazine/io/file/FileChannelDemo.java
@@ -0,0 +1,10 @@
+package com.tazine.io.file;
+
+/**
+ * FileChannel Practice
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class FileChannelDemo {
+}
---
+++ codeplay-io/src/main/java/com/tazine/io/file/RandAccessFile.java
@@ -0,0 +1,48 @@
+package com.tazine.io.file;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+/**
+ * RandomAccessFile
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class RandAccessFile {
+
+
+    public static void main(String[] args) throws IOException {
+
+        String path = "/Users/lina/rand.txt";
+        RandomAccessFile raf = new RandomAccessFile(path, "rw");
+
+        String str = "中国";
+        for (byte b : str.getBytes()) {
+            System.out.print(b + " ");
+        }
+        System.out.println();
+        raf.write(str.getBytes());
+        raf.close();
+
+        RandomAccessFile file = new RandomAccessFile(path, "r");
+
+        byte[] bytes = new byte[3];
+        byte[] bs = new byte[6];
+        file.read(bytes);
+//        int a = file.read(bs);
+//        System.out.println(a + " ----");
+        System.out.println(new String(bytes));
+
+        file.read(bytes);
+
+        System.out.println(bytes.length);
+        System.out.println(new String(bytes));
+//        System.out.println(new String(bs));
+        file.close();
+
+
+    }
+
+
+}

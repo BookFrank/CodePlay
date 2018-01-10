b68f4527e73a719dc35cd93556f7bf8fb2d2afe4
Frank <bookfrank@foxmail.com>
2018-01-10 22:37:21 +0800

    feat (codeplay-algorithm) : add the shuffle cards algorithm.

---
+++ codeplay-algorithm/src/main/java/com/tazine/algorithm/shuffle/SimpShuffle.java
@@ -0,0 +1,23 @@
+package com.tazine.algorithm.shuffle;
+
+/**
+ * Created by lina on 2018/1/9.
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class SimpShuffle {
+
+    private static int[] cards = {1,2,3,4,5,6,7,8,9};
+
+    public static void swap(int[] arr, int i, int j){
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(Math.random());
+
+    }
+
+}

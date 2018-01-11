79db5262d06887372fadc05e1eabbcfef2de5202
Frank <bookfrank@foxmail.com>
2018-01-11 23:20:50 +0800

    feat (codeplay-io) : Non-blocking IO time server practice.

---
+++ codeplay-io/src/main/java/com/tazine/io/netty/nio/NioTimeClient.java
@@ -0,0 +1,13 @@
+package com.tazine.io.netty.nio;
+
+/**
+ * Created by lina on 2018/1/11.
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class NioTimeClient {
+
+
+
+}
---
+++ codeplay-io/src/main/java/com/tazine/io/netty/nio/NioTimeServer.java
@@ -0,0 +1,22 @@
+package com.tazine.io.netty.nio;
+
+import io.netty.channel.socket.ServerSocketChannel;
+
+/**
+ * NIO TimeServer
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class NioTimeServer {
+
+
+    public NioTimeServer(int port) {
+
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}

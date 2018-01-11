92f7a296fbfad0780e3d494be3b603e963248778
Frank <bookfrank@foxmail.com>
2018-01-11 23:18:27 +0800

    feat (codeplay-container) : MyHash class wait to implement and IteratorLive.

---
+++ codeplay-container/src/main/java/com/tazine/container/map/MyHash.java
@@ -0,0 +1,10 @@
+package com.tazine.container.map;
+
+/**
+ * Created by lina on 2018/1/11.
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class MyHash {
+}
---
+++ codeplay-container/src/main/java/com/tazine/container/utils/IteratorLive.java
@@ -0,0 +1,37 @@
+package com.tazine.container.utils;
+
+import java.util.*;
+
+/**
+ * Created by lina on 2018/1/11.
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class IteratorLive {
+
+
+    private static void display(Iterator it){
+        while (it.hasNext()){
+            System.out.print(it.next() + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Integer[] arr = {1,2,3,4,5};
+
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(arr));
+        List<Integer> list2 = new LinkedList<>(Arrays.asList(arr));
+        Set<Integer> list3 = new HashSet<>(Arrays.asList(arr));
+
+        display(list1.iterator());
+        display(list2.iterator());
+        display(list3.iterator());
+    }
+
+
+
+
+}

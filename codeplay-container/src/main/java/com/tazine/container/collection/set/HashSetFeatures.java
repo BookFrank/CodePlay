44fcbea0f294556cd5d47c11e872d46bfb14ed72
Frank <bookfrank@foxmail.com>
2018-01-18 17:47:30 +0800

    feat (codeplay-container) ：HashMap versus HashTable 。

--- codeplay-container/src/main/java/com/tazine/container/collection/set/HashSetFeatures.java
+++ codeplay-container/src/main/java/com/tazine/container/collection/set/HashSetFeatures.java
@@ -13,17 +13,36 @@ import java.util.Set;
 public class HashSetFeatures {
 
     public static void main(String[] args) {
-        Random rand = new Random(47);
-        Set<Integer> set = new HashSet<>();
 
-        for (int i = 0; i < 10000; i++) {
-            set.add(rand.nextInt(30));
+        /**
+         * HashSet 的底层是一个 HashMap，下面这种声明方式采取默认值：
+         * 该 set 所能存储的元素个数为 16*0.75=12个，默认是16个桶
+         */
+        Set<String> strSet = new HashSet<>();
+
+        /**
+         * 下面这个Set集合能存放的元素个数最大为 16*2 = 32 个
+         */
+        Set<String> cSet = new HashSet<>(16,2);
+
+        for (int i = 0 ; i < 20; i++){
+            strSet.add(String.valueOf(i));
         }
 
-        if (set.contains(20)){
-            System.out.println("底层是 map.contains(key)");
-        }
 
-        System.out.println(set);
+
+//
+//        Random rand = new Random(47);
+//        Set<Integer> set = new HashSet<>();
+//
+//        for (int i = 0; i < 10000; i++) {
+//            set.add(rand.nextInt(30));
+//        }
+//
+//        if (set.contains(20)){
+//            System.out.println("底层是 map.contains(key)");
+//        }
+//
+//        System.out.println(set);
     }
 }
---
+++ codeplay-container/src/main/java/com/tazine/container/map/HashMapFeatures.java
@@ -0,0 +1,31 @@
+package com.tazine.container.map;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+/**
+ * Created by lina on 2018/1/18.
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class HashMapFeatures {
+    public static void main(String[] args) {
+
+        Map<Integer, String> map = new HashMap<>();
+        Map<Integer, String> table = new Hashtable<>();
+
+        // HashMap 可以接受 null key 和 null value,
+        // 除了非同步和允许 null之外，HashMap几乎和HashTable一样
+        map.put(null, null);
+        // table.put(null,null);
+
+        map.put(1, "kobe");
+
+        for (Map.Entry<Integer, String> e : map.entrySet()) {
+            System.out.print(e.getKey() + " : " + e.getValue());
+            System.out.println();
+        }
+    }
+}

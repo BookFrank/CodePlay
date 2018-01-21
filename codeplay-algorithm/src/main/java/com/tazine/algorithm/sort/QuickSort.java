2e1763433ab0dd547e6f0714a4179a5dcd661b9b
Frank <bookfrank@foxmail.com>
2018-01-21 23:51:56 +0800

    feat (codeplay-algorithm) : add the QuickSort practice.

--- codeplay-algorithm/src/main/java/com/tazine/algorithm/sort/QuickSort.java
+++ codeplay-algorithm/src/main/java/com/tazine/algorithm/sort/QuickSort.java
@@ -3,32 +3,50 @@ package com.tazine.algorithm.sort;
 import java.util.Arrays;
 
 /**
- * Created on 2017/10/12.
+ * QuickSort
  *
- * 快速排序
- *
- * @author Frank
+ * @author frank
+ * @since 1.0.0
  */
 public class QuickSort {
 
-    private static final int[] arr = {
-            5, 3, 4, 1, 2
-    };
-
-
-
     public static void main(String[] args) {
 
+        int[] arr = {5, 1, 4, 3, 2};
 
-
-
-        Arrays.sort(arr);
-
-        for (int i : arr){
-            System.out.println(i);
-        }
-
+        //Arrays.sort(arr);
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
     }
 
+    private static void quickSort(int[] arr, int left, int right) {
+        int f = arr[(left+right)/2];//被选中的临界值
+        int ltemp = left;
+        int rtemp = right;
 
+        while (ltemp < rtemp){
+            while (arr[ltemp] < f){
+                ++ltemp;
+            }
+            while (arr[rtemp] > f){
+                --rtemp;
+            }
+            if (ltemp <= rtemp){
+                int tmp = arr[ltemp];
+                arr[ltemp] = arr[rtemp];
+                arr[rtemp] = tmp;
+                --rtemp;
+                ++ltemp;
+            }
+            if (ltemp == rtemp){
+                ltemp++;
+            }
+            if (left < rtemp){
+                quickSort(arr,left, ltemp-1);
+            }
+            if (ltemp<right){
+                quickSort(arr, rtemp+1,right);
+            }
+        }
+    }
 }

fce3655fb50c14d6db562a290a78d2e0631784e7
Frank <bookfrank@foxmail.com>
2018-01-26 23:48:29 +0800

    feat (codeplay-base) : add the object features.

---
+++ codeplay-base/src/main/java/com/tazine/base/object/ObjectFeatures.java
@@ -0,0 +1,23 @@
+package com.tazine.base.object;
+
+/**
+ * ObjectFeatures
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class ObjectFeatures {
+
+    public static void main(String[] args) {
+
+        Object obj = new Object();
+        Object obj1 = obj;
+        Object obj2 = obj;
+
+        // 比较的是真实对象的 地址值，而不是两个引用相等不相等
+        System.out.println(obj1.equals(obj2));
+        System.out.println(obj1 == obj2);
+
+    }
+
+}

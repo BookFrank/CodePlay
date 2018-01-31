d5e4183d8ef114dac7d75e6aaee807051efb39c4
Frank <bookfrank@foxmail.com>
2018-01-31 21:54:47 +0800

    test (codeplay-container) : add and delete the container test.

--- codeplay-container/src/main/java/com/tazine/container/collection/Test.java
+++ codeplay-container/src/main/java/com/tazine/container/collection/Test.java
@@ -1,16 +1,20 @@
 package com.tazine.container.collection;
 
+
+/**
+ * Collection Test
+ *
+ * @author frank
+ * @since 1.0.0
+ */
 public class Test {
 
     public static void main(String[] args) {
-
-        Object o  = new Object();
+        Object o = new Object();
         System.out.println(Integer.toBinaryString(o.hashCode()));
         int h;
         int f = (h = o.hashCode()) ^ (h >>> 16);
         System.out.println(Integer.toBinaryString(f));
         System.out.println(Integer.toBinaryString(-1));
-
     }
-
 }

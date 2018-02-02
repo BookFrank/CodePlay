693105cec59e215f2db831f295e2d84ac067ac95
Frank <bookfrank@foxmail.com>
2018-02-02 11:08:01 +0800

    test (codeplay-io) : add the scanner test.

--- codeplay-io/src/main/java/com/tazine/io/socket/Test.java
+++ codeplay-io/src/main/java/com/tazine/io/socket/Test.java
@@ -3,7 +3,7 @@ package com.tazine.io.socket;
 import java.util.Scanner;
 
 /**
- * Created by lina on 2018/1/4.
+ * Scanner Test
  *
  * @author frank
  * @since 1.0.0
@@ -11,13 +11,9 @@ import java.util.Scanner;
 public class Test {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()){
+        while (sc.hasNext()) {
             System.out.println(sc.next());
         }
-
-
     }
-
 }

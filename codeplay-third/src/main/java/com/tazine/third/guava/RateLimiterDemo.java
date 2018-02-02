441f3df8590369b058651a7c03e8a650fa4a7045
Frank <bookfrank@foxmail.com>
2018-02-02 23:28:28 +0800

    test (codeplay-third) : add the guava test.

--- codeplay-third/src/main/java/com/tazine/third/guava/RateLimiterDemo.java
+++ codeplay-third/src/main/java/com/tazine/third/guava/RateLimiterDemo.java
@@ -2,8 +2,13 @@ package com.tazine.third.guava;
 
 import com.google.common.util.concurrent.RateLimiter;
 
+import java.util.Scanner;
+
 /**
  * Guava-限流
+ *
+ * @author frank
+ * @since 1.0.0
  */
 public class RateLimiterDemo {
 
@@ -12,9 +17,11 @@ public class RateLimiterDemo {
         // 创建一个每秒流量为2的令牌桶
         RateLimiter limiter = RateLimiter.create(2);
 
+        Scanner sc = new Scanner(System.in);
 
-
+        while (sc.hasNext()) {
+            //System.out.println(sc.next());
+            System.out.println("-- " + limiter.tryAcquire());
+        }
     }
-
-
 }

9c80923b773ff617ec9c8fa9237744eaa0a92b3e
Frank <bookfrank@foxmail.com>
2018-02-04 22:06:55 +0800

    feat (codeplay-third) : add the Guava Option practice.

---
+++ codeplay-third/src/main/java/com/tazine/third/guava/optional/OptionalDemo.java
@@ -0,0 +1,45 @@
+package com.tazine.third.guava.optional;
+
+import com.google.common.base.Optional;
+
+/**
+ * Guava Optional<T>
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class OptionalDemo {
+
+    public static void main(String[] args) {
+
+        Optional<Integer> val = Optional.of(5);
+        System.out.println("Optional<Integer> val = Optional.of(5)");
+        System.out.println("val.isPresent() = " + val.isPresent());
+        System.out.println("val.orNull() = " + val.orNull());
+        System.out.println("val.get() = " + val.get());
+        System.out.println();
+
+        val = Optional.fromNullable(null);
+        System.out.println("val = Optional.fromNullable(null)");
+        System.out.println("val.isPresent() = " + val.isPresent());
+        System.out.println("val.orNull() = " + val.orNull());
+        /**
+         * 如果 Optional 为 Null 将触发异常，无法向下执行
+         */
+        // System.out.println("val.get() = " + val.get());
+        System.out.println();
+        val = Optional.of(5);
+
+        // 创建引用缺失的 Optional 实例
+        val = Optional.absent();
+        System.out.println("val = Optional.absent()");
+        System.out.println("val.isPresent() = " + val.isPresent());
+        System.out.println("val.orNull() = " + val.orNull());
+        System.out.println();
+
+        // Optional 使用方式
+        Optional<Integer> op = Optional.fromNullable(null);
+        int a = op.or(21);
+        System.out.println(a);
+    }
+}

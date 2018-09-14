37693948f6e042c5caed5f141c3a8afb7536ebb0
Frank <bookfrank@foxmail.com>
2018-09-14 23:46:30 +0800

    feat (java8) : 增加 Lambda List 转 Map 实践。

--- codeplay-java8/src/main/java/com/tazine/java8/lambda/Test.java
+++ codeplay-java8/src/main/java/com/tazine/java8/lambda/Test.java
@@ -3,6 +3,7 @@ package com.tazine.java8.lambda;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -30,6 +31,8 @@ public class Test {
         List<String> names = list.stream().map(Player::getName).collect(Collectors.toList());
         System.out.println(names);
 
-
+        // List 转 Map
+        Map<String, Integer> map = list.stream().collect(Collectors.toMap(Player::getName, Player::getNum));
+        System.out.println(map);
     }
 }

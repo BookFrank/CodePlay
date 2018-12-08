250a3b101b601ed0292e682d90773521bd4057e0
Frank <bookfrank@foxmail.com>
2018-12-08 23:47:17 +0800

    feat (logback) : Logback context 测试。

--- codeplay-logback/src/main/java/com/tazine/codeplay/logback/Test.java
+++ codeplay-logback/src/main/java/com/tazine/codeplay/logback/Test.java
@@ -1,5 +1,7 @@
 package com.tazine.codeplay.logback;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +16,10 @@ public class Test {
     private static final Logger logger = LoggerFactory.getLogger(Test.class);
 
     public static void main(String[] args) {
-        logger.info("日志测试 - {}", "Hello World");
+        logger.info("日志测试  - {}", "Hello World");
+
+        // 打印 Logger 状态 print internal state
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        StatusPrinter.print(loggerContext);
     }
 }

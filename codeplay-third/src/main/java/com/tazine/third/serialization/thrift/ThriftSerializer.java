373cb8160a8a441cdc3940c4b5593d54eaa88f5b
Frank <bookfrank@foxmail.com>
2018-12-26 23:44:19 +0800

    feat (third) : thrift 序列化测试。

---
+++ codeplay-third/src/main/java/com/tazine/third/serialization/thrift/ThriftSerializer.java
@@ -0,0 +1,20 @@
+package com.tazine.third.serialization.thrift;
+
+import com.tazine.third.serialization.ISerializer;
+
+/**
+ * Created by lina on 2018/12/26.
+ *
+ * @author frank
+ * @date 2018/12/26
+ */
+public class ThriftSerializer implements ISerializer {
+
+    public <T> byte[] serialize(T obj) {
+        return new byte[0];
+    }
+
+    public <T> T deserialize(byte[] data, Class<T> clz) {
+        return null;
+    }
+}

53adccfadf1550940596ee72250cc581df442dd8
Frank <bookfrank@foxmail.com>
2018-12-26 23:44:09 +0800

    feat (third) : avro 序列化测试。

---
+++ codeplay-third/src/main/java/com/tazine/third/serialization/avro/AvroSerializer.java
@@ -0,0 +1,20 @@
+package com.tazine.third.serialization.avro;
+
+import com.tazine.third.serialization.ISerializer;
+
+/**
+ * Created by lina on 2018/12/26.
+ *
+ * @author frank
+ * @date 2018/12/26
+ */
+public class AvroSerializer implements ISerializer {
+
+    public <T> byte[] serialize(T obj) {
+        return new byte[0];
+    }
+
+    public <T> T deserialize(byte[] data, Class<T> clz) {
+        return null;
+    }
+}

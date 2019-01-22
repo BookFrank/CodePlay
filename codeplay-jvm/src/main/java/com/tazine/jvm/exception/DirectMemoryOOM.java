7ff94fcfc032c6f1d41b44eeb0ea3cbe5fa26b27
Frank <bookfrank@foxmail.com>
2019-01-22 15:15:46 +0800

    feat (jvm) : direct memory oom.

---
+++ codeplay-jvm/src/main/java/com/tazine/jvm/exception/DirectMemoryOOM.java
@@ -0,0 +1,27 @@
+package com.tazine.jvm.exception;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+/**
+ * 直接内存溢出
+ * VM Args: -Xmx20M -XX:MaxDirectMemorySize=10M
+ *
+ * @author frank
+ * @date 2019/01/22
+ */
+public class DirectMemoryOOM {
+
+    private static final int _1MB = 1024 * 1024;
+
+    public static void main(String[] args) throws IllegalAccessException {
+        Field unsafeField = Unsafe.class.getDeclaredFields()[0];
+        unsafeField.setAccessible(true);
+        Unsafe unsafe = (Unsafe)unsafeField.get(null);
+        while (true){
+            unsafe.allocateMemory(_1MB);
+        }
+    }
+
+}

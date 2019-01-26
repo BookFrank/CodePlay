ffc3f4607a00a1628f447974d0553dac364e57ea
Frank <bookfrank@foxmail.com>
2019-01-26 23:16:58 +0800

    feat (jvm) : CPU 使用率过高。

---
+++ codeplay-jvm/src/main/java/com/tazine/jvm/exception/CpuOveruse.java
@@ -0,0 +1,41 @@
+package com.tazine.jvm.exception;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * CPU 使用率过高
+ *
+ * @author frank
+ * @date 2019/01/26
+ */
+public class CpuOveruse {
+
+    private static Executor executor = Executors.newFixedThreadPool(4);
+
+    private static Object lock = new Object();
+
+    public static void main(String[] args) {
+        Task task1 = new Task();
+        Task task2 = new Task();
+
+        executor.execute(task1);
+        executor.execute(task2);
+    }
+
+    private static class Task implements Runnable{
+        @Override
+        public void run() {
+            synchronized (lock){
+                doBusiness();
+            }
+        }
+
+        public void doBusiness(){
+            int i = 0;
+            while (true){
+                i++;
+            }
+        }
+    }
+}

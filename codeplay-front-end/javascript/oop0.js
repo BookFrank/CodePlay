e75af30df23c2c767d9ca5b5763b1c4cc1ebe675
Frank <bookfrank@foxmail.com>
2019-02-14 15:29:48 +0800

    feat (front) : 增加js对象测试。

--- codeplay-front-end/javascript/oop0.js
+++ codeplay-front-end/javascript/oop0.js
@@ -38,12 +38,16 @@ Player.prototype.sayHello = function(){
 var kobe = new Player("kobe");
 var hiFunc = kobe.sayHello;
 
-// 在 Javascript 中，方法通常是一个绑定到对象中的普通函数，这意味着方法可以再其所在的 context 之外被调用
-
-
 kobe.sayHello();
 hiFunc.call(kobe);
 hiFunc(); // Hi, I am undfined
+
+// 在 Javascript 中，方法通常是一个绑定到对象中的普通函数，这意味着方法可以再其所在的 context 之外被调用
+// 上面所有的使用方式，都指向了 sayHello 函数的引用；
+// 在调用函数的过程中，this的值取决于我们怎样调用函数，在通常情况下，我们通过一个表达式 kobe.sayHello() 来调用函数，
+// 即从一个对象的属性中得到所调用的函数，此时 this 被设置为我们去的函数的对象即 kobe。
+
+// 不同的调用方法，this 值也不相同。当从变量 hiFunc() 中调用的时候，this就被设置成了全局对象，在浏览器中就是window。
 
 console.log(hiFunc === kobe.sayHello); // true
 console.log(hiFunc === Player.prototype.sayHello); // true

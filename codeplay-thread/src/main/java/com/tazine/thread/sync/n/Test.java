1fd28a1fae097070c45ba885a731860917e0a34f
Frank <bookfrank@foxmail.com>
2019-02-19 23:48:07 +0800

    feat (thread) : 线程的同步。

--- codeplay-thread/src/main/java/com/tazine/thread/sync/n/Test.java
+++ codeplay-thread/src/main/java/com/tazine/thread/sync/n/Test.java
@@ -1,18 +1,20 @@
 package com.tazine.thread.sync.n;
 
 /**
- * Created by lina on 2017/9/7.
+ * Test
+ *
+ * @author frank
+ * @date 2017/9/7
  */
 public class Test {
-
 
     public static void main(String[] args) {
 
         Thread01 thread01 = new Thread01();
 
-        new Thread(thread01,"A").start();
+        new Thread(thread01, "A").start();
 
-        new Thread(thread01,"B").start();
+        new Thread(thread01, "B").start();
 
 //        new Thread(new Thread01("C")).start();
 
--- codeplay-thread/src/main/java/com/tazine/thread/sync/n/Thread01.java
+++ codeplay-thread/src/main/java/com/tazine/thread/sync/n/Thread01.java
@@ -1,8 +1,10 @@
 package com.tazine.thread.sync.n;
 
 /**
- * Created by ly on 2017/9/7.
  * 把自己锁住，同一时间只会有一个在执行
+ *
+ * @author frank
+ * @date 2017/9/7
  */
 public class Thread01 implements Runnable {
 
@@ -10,8 +12,10 @@ public class Thread01 implements Runnable {
     public void run() {
         // 把自己锁住，可以保证同一时间最多有一个线程执行
         // run部分的代码必须全部执行完成才会走下一个线程
-        synchronized (this){
-            for (int i = 1; i< 6; i ++){
+
+        System.out.println(this.getClass().getCanonicalName());
+        synchronized (this) {
+            for (int i = 1; i < 6; i++) {
                 System.out.println(Thread.currentThread().getName() + "---" + i);
                 try {
                     Thread.sleep(200);

71b84bf977abb6a0d360acc6a6f6b152abd85256
Frank <bookfrank@foxmail.com>
2019-02-23 23:17:27 +0800

    feat (thread) : 线程的 yield 。

--- codeplay-thread/src/main/java/com/tazine/thread/action/YieldDemo.java
+++ codeplay-thread/src/main/java/com/tazine/thread/action/YieldDemo.java
@@ -2,29 +2,37 @@ package com.tazine.thread.action;
 
 /**
  * Thread.yield()：可以将CPU从当前线程转移给另一个线程
+ * 使当前线程从执行状态（运行状态）变为可执行态（就绪状态）
  *
  * @author frank
  * @date 2018/07/26
  */
 public class YieldDemo {
 
-    public static void main(String[] args) {
-        YieldThread thread = new YieldThread();
-        thread.start();
+    public static void main(String[] args) throws InterruptedException {
+        YieldThread yt1 = new YieldThread("hello");
+        YieldThread yt2 = new YieldThread("--world");
+        yt1.start();
+        yt2.start();
     }
 
 }
 
 
 class YieldThread extends Thread {
+
+    public YieldThread(String name) {
+        super(name);
+    }
+
     @Override
     public void run() {
-        System.out.println("YieldThread 正在执行......");
-        try {
-            sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (int i = 1; i <= 50; i++) {
+            System.out.println("" + this.getName() + "-----" + i);
+            // 当i为30时，该线程就会把CPU时间让掉，让其他或者自己的线程执行（也就是谁先抢到谁执行）
+            if (i == 30) {
+                Thread.yield();
+            }
         }
-        yield();
     }
 }

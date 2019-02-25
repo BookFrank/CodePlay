04d56c0ff3cc50ba370b79da80f4f5f1cac55929
Frank <bookfrank@foxmail.com>
2019-02-25 23:24:38 +0800

    feat (thread) : CachedThreadPool 使用 review。

---
+++ codeplay-thread/src/main/java/com/tazine/thread/pool/CachedThreadPool.java
@@ -0,0 +1,27 @@
+package com.tazine.thread.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Executors.newCachedThreadPool()
+ *
+ * @author frank
+ * @date 2017/12/26
+ */
+public class CachedThreadPool {
+
+    /**
+     * shundown() 方法可以防止新任务被提交给这个 Executor，当前线程（main（）线程）将继续运行在 shuntdown()
+     * 被调用之前提交的所有任务
+     */
+
+    public static void main(String[] args) {
+
+        ExecutorService pool = Executors.newCachedThreadPool();
+        for (int i = 0; i < 3; i++) {
+            pool.submit(new PrinterThread());
+        }
+        pool.shutdown();
+    }
+}

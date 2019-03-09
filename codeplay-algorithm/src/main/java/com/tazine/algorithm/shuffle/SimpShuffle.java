984071835c73515063d1df143424d482e920dd7b
Frank <bookfrank@foxmail.com>
2019-03-09 23:47:22 +0800

    doc (algorithm) : 注释优化。

--- codeplay-algorithm/src/main/java/com/tazine/algorithm/shuffle/SimpShuffle.java
+++ codeplay-algorithm/src/main/java/com/tazine/algorithm/shuffle/SimpShuffle.java
@@ -1,10 +1,10 @@
 package com.tazine.algorithm.shuffle;
 
 /**
- * Created by lina on 2018/1/9.
+ * SimpShuffle
  *
  * @author frank
- * @since 1.0.0
+ * @date 2018/1/9
  */
 public class SimpShuffle {
 

9b45f37bdd461e20fd1af394e3b34c94d12eb18e
Frank <bookfrank@foxmail.com>
2019-03-13 21:26:31 +0800

    feat (database) : 增加原生调用。

---
+++ codeplay-database/src/main/java/com/tazine/codeplay/db/raw/App.java
@@ -0,0 +1,33 @@
+package com.tazine.codeplay.db.raw;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+/**
+ * 使用 mysql 驱动连接 MySQL数据库
+ *
+ * @author frank
+ * @date 2017/03/13
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "");
+
+            ResultSet rt = connection.prepareStatement("SELECT * FROM player").executeQuery();
+
+            while (rt.next()) {
+                System.out.println(rt.getString("name"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}

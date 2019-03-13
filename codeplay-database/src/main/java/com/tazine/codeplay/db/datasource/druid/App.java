48b1e44fb7c7eb726e65b7f4e1395b5dc1035039
Frank <bookfrank@foxmail.com>
2019-03-13 23:58:44 +0800

    feat (db) : add druid datasource demo.

---
+++ codeplay-database/src/main/java/com/tazine/codeplay/db/datasource/druid/App.java
@@ -0,0 +1,13 @@
+package com.tazine.codeplay.db.datasource.druid;
+
+/**
+ * Created by lina on 2019/3/13.
+ *
+ * @author frank
+ * @date 2019/3/13
+ */
+public class App {
+
+
+
+}
---
+++ codeplay-database/src/main/java/com/tazine/codeplay/db/datasource/druid/DruidDataSourceHolder.java
@@ -0,0 +1,17 @@
+package com.tazine.codeplay.db.datasource.druid;
+
+import com.alibaba.druid.pool.DruidDataSource;
+
+/**
+ * Created by lina on 2019/3/13.
+ *
+ * @author frank
+ * @date 2019/3/13
+ */
+public class DruidDataSourceHolder {
+
+
+    private static DruidDataSource dataSource;
+
+
+}

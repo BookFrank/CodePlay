c6b5d810b03f8534c71be620a654e664f7baa881
Frank <bookfrank@foxmail.com>
2019-03-25 23:44:39 +0800

    feat (third) : 将 logback 模块移至 third 模块。

---
+++ codeplay-third/src/main/java/com/tazine/third/logback/Test.java
@@ -0,0 +1,27 @@
+package com.tazine.third.logback;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Logback 测试
+ *
+ * @author frank
+ * @date 2018/12/08
+ */
+public class Test {
+
+    private static final Logger logger = LoggerFactory.getLogger(Test.class);
+
+    Logger rootLogger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+
+    public static void main(String[] args) {
+        // 打印 Logger 状态 print internal state
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        StatusPrinter.print(loggerContext);
+
+        logger.info("日志测试  - {}", "Hello World");
+    }
+}

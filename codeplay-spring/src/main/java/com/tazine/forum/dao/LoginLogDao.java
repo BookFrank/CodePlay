2cbc493322336c3f50329f83b0fa2ff5971c5d40
Frank <bookfrank@foxmail.com>
2019-03-30 23:44:34 +0800

    doc (spring) : 代码注释修改。

--- codeplay-spring/src/main/java/com/tazine/forum/dao/LoginLogDao.java
+++ codeplay-spring/src/main/java/com/tazine/forum/dao/LoginLogDao.java
@@ -5,7 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 /**
- * Created by lina on 2017/10/9.
+ * LoginLogDao
  *
  * @author frank
  * @date 2017/10/09

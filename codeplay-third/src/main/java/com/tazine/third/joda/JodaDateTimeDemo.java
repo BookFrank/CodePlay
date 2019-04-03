d63a83e1353169efe96973e7023a9885bacb38d1
Frank <bookfrank@foxmail.com>
2019-04-03 23:35:07 +0800

    feat (third) : joda.

---
+++ codeplay-third/src/main/java/com/tazine/third/joda/JodaDateTimeDemo.java
@@ -0,0 +1,21 @@
+package com.tazine.third.joda;
+
+import org.joda.time.DateTime;
+
+/**
+ * JodaDateTimeDemo
+ *
+ * @author frank
+ * @date 2018/03/22
+ */
+public class JodaDateTimeDemo {
+
+    public static void main(String[] args) {
+
+        String s = new DateTime().toString("yyyyMMddhhmmss");
+        System.out.println(s);
+
+
+    }
+
+}

a35ab45c1441c1ec7ca25b72c65f8ccb1256a788
Frank <bookfrank@foxmail.com>
2019-04-18 23:50:06 +0800

    feat (version) : map reduce.

---
+++ codeplay-java-version/src/main/java/com/tazine/codeplay/version/Student.java
@@ -0,0 +1,19 @@
+package com.tazine.codeplay.version;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Created by frank on 2019-04-18.
+ *
+ * @author frank
+ * @date 2019-04-18
+ */
+@Data
+@AllArgsConstructor
+public class Student {
+
+    private String name;
+
+    private Integer score;
+}
---
+++ codeplay-java-version/src/main/java/com/tazine/codeplay/version/java8/stream/MapReduceDemo.java
@@ -0,0 +1,31 @@
+package com.tazine.codeplay.version.java8.stream;
+
+import com.google.common.collect.Lists;
+import com.tazine.codeplay.version.Student;
+
+import java.util.List;
+
+/**
+ * Created by frank on 2019-04-18.
+ *
+ * @author frank
+ * @date 2019-04-18
+ */
+public class MapReduceDemo {
+
+    public static void main(String[] args) {
+
+        Student zhangsan = new Student("zhangsan", 92);
+        Student lisi = new Student("lisi", 89);
+        Student wangwu = new Student("wangwu", 98);
+
+        List<Student> students = Lists.newArrayList(zhangsan, lisi, wangwu);
+
+
+        
+
+
+
+    }
+
+}

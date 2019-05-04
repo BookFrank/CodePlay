b3577f84cc49b4d1610129f6b04b5ce22c0494bf
Frank <bookfrank@foxmail.com>
2019-05-04 20:46:42 +0800

    feat (version) : stream review。

--- codeplay-java-version/src/main/java/com/tazine/codeplay/version/NbaPlayer.java
+++ codeplay-java-version/src/main/java/com/tazine/codeplay/version/NbaPlayer.java
@@ -19,8 +19,18 @@ public class NbaPlayer {
 
     private String team;
 
+    private String section;
+
+    private Integer age;
+
     public NbaPlayer(String name, int num) {
         this.name = name;
         this.num = num;
+    }
+
+    public NbaPlayer(String name, int num, String team) {
+        this.name = name;
+        this.num = num;
+        this.team = team;
     }
 }
--- codeplay-java-version/src/main/java/com/tazine/codeplay/version/java8/stream/StreamDemo.java
+++ codeplay-java-version/src/main/java/com/tazine/codeplay/version/java8/stream/StreamDemo.java
@@ -5,6 +5,7 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.tazine.codeplay.version.NbaPlayer;
 
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -30,19 +31,34 @@ public class StreamDemo {
 
         // 1. 分组
         Map<String, List<NbaPlayer>> group = allstars.stream().collect(Collectors.groupingBy(NbaPlayer::getTeam));
-        System.out.println(JSON.toJSONString(group));
-        // 1.1 如果分组的属性是唯一的，上面的代码就很麻烦，可以使用Guava
+        // 2. 唯一属性分组，如果分组的属性是唯一的，上面的代码就很麻烦，可以使用Guava
         Map<String, NbaPlayer> starsMap = Maps.uniqueIndex(allstars, NbaPlayer::getName);
-        System.out.println(starsMap);
-        // 1.2 获取不同分组属性下的某一属性值
-        Map<String, List<Integer>> group4 = allstars.stream().collect(
-            Collectors.groupingBy(NbaPlayer::getTeam, Collectors.mapping(NbaPlayer::getNum, Collectors.toList())));
-        System.out.println(group4);
-
-        // 计数 根据某个属性分组计数
+        // 3. 分组计数，计算各个属性分组计数
         Map<String, Long> group1 = allstars.stream().collect(
             Collectors.groupingBy(NbaPlayer::getTeam, Collectors.counting()));
+        // 4. 非唯一属性分组，获取不同分组属性下的某一属性值
+        Map<String, List<String>> group4 = allstars.stream().collect(
+            Collectors.groupingBy(NbaPlayer::getTeam, Collectors.mapping(NbaPlayer::getName, Collectors.toList())));
+        // 5. 二级分组
+        Map<String, Map<String, List<NbaPlayer>>> group5 = allstars.stream().collect(
+            Collectors.groupingBy(NbaPlayer::getSection, Collectors.groupingBy(NbaPlayer::getTeam)));
+        // 6. 属性平均值
+        double avgAge = allstars.stream().collect(Collectors.averagingInt(NbaPlayer::getAge));
+        // 7. 属性和
+        int totalAge = allstars.stream().collect(Collectors.summingInt(NbaPlayer::getAge));
+        // 8. 一次性获取元素个数、总和、均值、最大值、最小值
+        IntSummaryStatistics summary = allstars.stream().collect(Collectors.summarizingInt(NbaPlayer::getAge));
+        // IntSummaryStatistics{count=24, sum=482, min=18, average=23, max=36}
+
+
+        System.out.println(JSON.toJSONString(group));
+        System.out.println(starsMap);
+        System.out.println(group4);
+        System.out.println(group5);
         System.out.println(group1);
+        System.out.println(avgAge);
+        System.out.println(totalAge);
+        System.out.println(summary);
 
         // 根据整个实体对象分组计数，当其为string时常使用
         Map<NbaPlayer, Long> group2 = allstars.stream().collect(

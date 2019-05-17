dafc5b47d5cc07d907a6cec5c335f481d373295c
Frank <bookfrank@foxmail.com>
2019-05-17 23:57:48 +0800

    feat (thread) : re entry review.

--- codeplay-thread/src/main/java/com/tazine/thread/lock/spinlock/ReSpinLock.java
+++ codeplay-thread/src/main/java/com/tazine/thread/lock/spinlock/ReSpinLock.java
@@ -1,29 +1,36 @@
-package com.tazine.thread.lock.synchron;
+package com.tazine.thread.lock.spinlock;
 
 /**
- * ReSync
- *
  * @author frank
  * @date 2019/05/17
  */
-public class ReSync {
+public class ReSpinLock {
 
-    public synchronized void a(){
+    private SpinLock lock = new SpinLock();
+
+    public void a(){
         try {
+            lock.lock();
             Thread.sleep(2000);
             System.out.println(Thread.currentThread().getName() + " 执行 a 方法");
             b();
         } catch (InterruptedException e) {
             e.printStackTrace();
+        } finally {
+            lock.unlock();
         }
     }
 
     public synchronized void b(){
         try {
+            lock.lock();
             Thread.sleep(200);
             System.out.println(Thread.currentThread().getName() + " 执行 b 方法");
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }finally {
+            lock.unlock();
         }
     }
+
 }

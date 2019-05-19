3a0051400748b78cb0df86e45de82ce6c0d96db6
Frank <bookfrank@foxmail.com>
2019-05-19 22:42:36 +0800

    feat (thread) : lock review.

--- codeplay-thread/src/main/java/com/tazine/thread/lock/spinlock/ReSpinLock.java
+++ codeplay-thread/src/main/java/com/tazine/thread/lock/spinlock/ReSpinLock.java
@@ -1,6 +1,8 @@
 package com.tazine.thread.lock.spinlock;
 
 /**
+ * 基于自旋锁的锁重入
+ *
  * @author frank
  * @date 2019/05/17
  */
@@ -8,7 +10,7 @@ public class ReSpinLock {
 
     private SpinLock lock = new SpinLock();
 
-    public void a(){
+    public void a() {
         try {
             lock.lock();
             Thread.sleep(2000);
@@ -21,16 +23,15 @@ public class ReSpinLock {
         }
     }
 
-    public synchronized void b(){
+    public void b() {
         try {
             lock.lock();
             Thread.sleep(200);
             System.out.println(Thread.currentThread().getName() + " 执行 b 方法");
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             lock.unlock();
         }
     }
-
 }
